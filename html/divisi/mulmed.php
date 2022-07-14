cd5ee81adefb7e2ee3936d2fec782a6a16c54996
axrf <x9legaci@gmail.com>
2022-07-15 02:21:13 +0700

    Bios w/PHP

---
+++ html/divisi/mulmed.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>BIOS | Multimedia</title>
+    <link rel="shortcut icon" href="../../img/favicon.png" type="image/png">
+    <link rel="stylesheet" href="../../style/divisi/mulmed.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top" id="navbar">
+        <div class="container">
+            <a class="navbar-brand pb-3" href="../../index.php">
+                <img src="../../img/logonav.png" alt="Bios Logo" height="24">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../../index.php">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false"> Profil </a>
+                        <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item text-white" href="../profil/visimisi.php">Visi dan Misi</a>
+                            </li>
+                            <li><a class="dropdown-item text-white" href="../profil/sejarah.php">Sejarah Singkat</a>
+                            </li>
+                            <li><a class="dropdown-item text-white" href="../profil/struktur.php">Struktur
+                                    Organisasi</a></li>
+                            <li><a class="dropdown-item text-white" href="../profil/prestasi.php">Prestasi</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false"> Divisi </a>
+                        <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item text-white" href="mulmed.php">Multimedia</a></li>
+                            <li><a class="dropdown-item text-white" href="pca.php">PCA</a></li>
+                            <li><a class="dropdown-item text-white" href="program.php">Program</a></li>
+                            <li><a class="dropdown-item text-white" href="wb.php">Web Design</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../galeri.php">Galeri</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-md pb-5">
+        <div class="row justify-content-center">
+            <div class="col">
+                <h2 class="text-center">Divisi Multimedia</h2>
+                <p class="text-dark" align=" justify">
+                    Divisi multimedia adalah salah satu dari empat divisi yang ada di ekstrakulikuler BIOS. Divisi
+                    multimedia ini diketuai oleh M. Putra (periode 2021-2022). Pada divisi ini kamu akan mempelajari
+                    vidiografi, fotografi, pembuatan animasi dan pembuatan poster digital.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container-md pb-3">
+        <div class="row justify-content-center">
+            <div class="col pb-3">
+                <h3 class="text-center">Project Divisi Multimedia</h3>
+            </div>
+        </div>
+    </div>
+    <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+        <div class="col pb-3">
+            <div class="card">
+                <a href="../../project/mulmed1/FeedBios.png"><img src="../../project/mulmed1.png" class="card-img-top"
+                        alt="FeedBios"></a>
+                <div class="card-body">
+                    <h5 class="card-title">Feed Instagram</h5><br>
+                    <p class="card-text text-secondary"><i>Dibuat oleh: Zahra Yani</i></p>
+                    <a href="../../project/mulmed1/FeedBios.png" class="btn btn-dark">Lihat File Penuh</a>
+                </div>
+            </div>
+        </div>
+        <div class="col pb-3">
+            <div class="card">
+                <a href="../../project/mulmed2/mulmed2.mp4"><img src="../../project/mulmed2.jpg" class="card-img-top"
+                        alt="PosterAnimasi"></a>
+                <div class="card-body">
+                    <h5 class="card-title">Poster Animasi</h5><br>
+                    <p class="card-text text-secondary"><i>Dibuat oleh: Dimas Surmaaji</i></p>
+                    <a href="../../project/mulmed2/mulmed2.mp4" class="btn btn-dark">Lihat File Penuh</a>
+                </div>
+            </div>
+        </div>
+        <div class="col pb-3">
+            <div class="card">
+                <a href="../../project/mulmed3/mulmed3.jpg"><img src="../../project/mulmed3.jpg" class="card-img-top"
+                        alt="..."></a>
+                <div class="card-body">
+                    <h5 class="card-title">Poster Digital: Pancaroba Indonesia</h5><br>
+                    <p class="card-text text-secondary"><i>Dibuat oleh: Zahra Yani</i></p>
+                    <a href="../../project/mulmed3/mulmed3.jpg" class="btn btn-dark">Lihat File Penuh</a>
+                </div>
+            </div>
+        </div>
+        <div class="col pb-3">
+            <div class="card">
+                <a href="../../project/mulmed4/mulmed4.png"><img src="../../project/mulmed4.png" class="card-img-top"
+                        alt="..."></a>
+                <div class="card-body">
+                    <h5 class="card-title">Poster Digital: Build a Country with Smart Student Using Technology</h5>
+                    <p class="card-text text-secondary"><i>Dibuat oleh: Zahra Yani</i></p>
+                    <a href="../../project/mulmed4/mulmed4.png" class="btn btn-dark">Lihat File Penuh</a>
+                </div>
+            </div>
+        </div>
+        <div class="col pb-3">
+            <div class="card">
+                <a href="../../project/mulmed5/mulmed5.mp4"><img src="../../project/mulmed5.png" class="card-img-top"
+                        alt="..."></a>
+                <div class="card-body">
+                    <h5 class="card-title">Animasi</h5><br>
+                    <p class="card-text text-secondary"><i>Dibuat oleh: Zahra Yani</i></p>
+                    <a href="../../project/mulmed5/mulmed5.mp4" class="btn btn-dark">Lihat File Penuh</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Script Bootsrtap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
+    <!-- Akhir Script Bootstrap -->
+</body>
+
+</html>
---
+++ html/divisi/program.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>BIOS | Program</title>
+    <link rel="shortcut icon" href="../../img/favicon.png" type="image/png">
+    <link rel="stylesheet" href="../../style/divisi/program.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top" id="navbar">
+        <div class="container">
+            <a class="navbar-brand pb-3" href="../../index.php">
+                <img src="../../img/logonav.png" alt="Bios Logo" height="24">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../../index.php">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false"> Profil </a>
+                        <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item text-white" href="../profil/visimisi.php">Visi dan Misi</a>
+                            </li>
+                            <li><a class="dropdown-item text-white" href="../profil/sejarah.php">Sejarah Singkat</a>
+                            </li>
+                            <li><a class="dropdown-item text-white" href="../profil/struktur.php">Struktur
+                                    Organisasi</a></li>
+                            <li><a class="dropdown-item text-white" href="../profil/prestasi.php">Prestasi</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false"> Divisi </a>
+                        <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item text-white" href="mulmed.php">Multimedia</a></li>
+                            <li><a class="dropdown-item text-white" href="pca.php">PCA</a></li>
+                            <li><a class="dropdown-item text-white" href="program.php">Program</a></li>
+                            <li><a class="dropdown-item text-white" href="wb.php">Web Design</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../galeri.php">Galeri</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-md pb-5">
+        <div class="row justify-content-center">
+            <div class="col">
+                <h2 class="text-center">Divisi Program</h2>
+                <p class="text-dark" align=" justify">
+                    Divisi program adalah salah satu dari empat divisi yang ada di ekstrakulikuler BIOS. Divisi
+                    program ini diketuai oleh Annisa A. (periode 2021-2022). Pada divisi ini kamu akan mempelajari
+                    pemrograman tentunya, mulai dari dasar seperti HTML, CSS sampai JavaScript dan bahasa pemrograman
+                    lainnya seperti C, C++, C#, Phyton, dan lain-lain.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container-md pb-3">
+        <div class="row justify-content-center">
+            <div class="col">
+                <h3 class="text-center">Project Divisi Program</h3>
+            </div>
+        </div>
+    </div>
+    <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+        <div class="col pb-3">
+            <div class="card">
+                <a href="../../project/program1/index.php"><img src="../../project/program1.png" class="card-img-top"
+                        alt="..."></a>
+                <div class="card-body">
+                    <h5 class="card-title">Kalkulator HTML & CSS Only</h5>
+                    <p class="card-text text-secondary"><i>Dibuat oleh: Arief F-sa Wijaya</i></p>
+                    <a href="../../project/program1/index.php" class="btn btn-dark">Lihat File Penuh</a>
+                </div>
+            </div>
+        </div>
+        <div class="col pb-3">
+            <div class="card">
+                <a href="../../project/program2/index.php"><img src="../../project/program2.png" class="card-img-top"
+                        alt="..."></a>
+                <div class="card-body">
+                    <h5 class="card-title">Kalkulator Menggunakan JavaScript</h5>
+                    <p class="card-text text-secondary"><i>Dibuat oleh: Annisa A.</i></p>
+                    <a href="../../project/program2/index.php" class="btn btn-dark">Lihat File Penuh</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Script Bootsrtap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
+    <!-- Akhir Script Bootstrap -->
+</body>
+
+</html>
